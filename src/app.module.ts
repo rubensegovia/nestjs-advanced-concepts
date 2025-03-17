761e943f0b880beea2b502af8c9f508711466621
Rubén Carrasco <rubencf@ext.inditex.com>
2025-03-17 17:53:37 +0100

    feat: DI subtrees and events

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,27 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CronModule } from './cron/cron.module';
 import { FibonacciModule } from './fibonacci/fibonacci.module';
 import { HttpClientModule } from './http-client/http-client.module';
-import { SchedulerModule } from './scheduler/scheduler.module';
 import { RecipesModule } from './recipes/recipes.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
+import { TagsModule } from './tags/tags.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot(),
     CoffeesModule,
     SchedulerModule,
     CronModule,
     FibonacciModule,
     HttpClientModule.register({ baseUrl: 'http://nestjs.com' }),
     RecipesModule,
+    TagsModule,
+    PaymentsModule,
     // ⚠️  Alternatively:
     // HttpClientModule.registerAsync({
     //   useFactory: () => ({ baseUrl: 'http://nestjs.com' }),
